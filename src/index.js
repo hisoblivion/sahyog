c74ee2ff599b15c3d17b119cb85f2b64ae57c9c5
Nitish Kumar <nitish.glances@gmail.com>
2025-03-02 03:27:20 +0530

    Updated website

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <HashRouter>
+    <App />
+  </HashRouter>,
+  document.getElementById("root")
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
