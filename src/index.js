63d68eb623879192410226262b51a1294a68c2b5
Nitish Kumar <nitish.glances@gmail.com>
2025-03-02 03:37:08 +0530

    Updated website

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
+
+const root = createRoot(document.getElementById("root"));
+root.render(
   <HashRouter>
     <App />
-  </HashRouter>,
-  document.getElementById("root")
+  </HashRouter>
 );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
